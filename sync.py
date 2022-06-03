75aec22426e95ebfe50fbea7ef2f645ef2c2ee03
= <=>
2022-06-03 10:12:52 +0000

    Minification auto du fichier css

--- sync.py
+++ sync.py
@@ -1,20 +1,28 @@
 import os, time, shutil
 x = 1
 
-V1 = str(os.stat("web-design.html").st_atime)
-
+HTML_FILE = str(os.stat("web-design.html").st_atime)
+CSS_FILE = str(os.stat("style.css").st_atime)
 
 while x:
     html_meta_data = os.stat("web-design.html")
+    css_meta_data = os.stat("style.css")
     time.sleep(2)
 
-    if V1 != str(html_meta_data.st_atime) :
+    if HTML_FILE != str(html_meta_data.st_atime) :
         print("Actualisation de index.html")
         #Suppression du fichier index
         os.remove("index.html")
         time.sleep(1)
         #Création d'un nouveau fichier index
         shutil.copyfile("web-design.html", "index.html")
-        V1 = str(os.stat("web-design.html").st_atime)
+        HTML_FILE = str(os.stat("web-design.html").st_atime)
+    else :
+        pass
+
+    if CSS_FILE != str(css_meta_data.st_atime) :
+        print("Actualisation de style.min.css")
+        os.system("css-minify -f style.css -o .")
+        CSS_FILE = str(os.stat("style.css").st_atime)
     else :
         pass
